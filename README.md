f90f190fbd91ac120758e9453cb7ec19158d5025
jismartin <jismartin@gmail.com>
2025-01-21 13:46:52 +0100

    Update all code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nextw
-A web application that shows the autoregression mechanism for predicting the next word
+A minimalistic web application that shows the autoregression mechanism for predicting the next word.
---
+++ main.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import openai
+
+# Load OpenAI client
+@st.cache_resource
+def create_openai_client(apikey):
+    return openai.Client(api_key=apikey)
+
+@st.cache_data
+def generate_text(input_text, num_words, temperature):
+    
+    client = create_openai_client(st.secrets["my_secrets"]["openai_key"])
+
+    prompt = input_text
+    #top_tokens_list = []
+    response_list=[]
+    generated_text = prompt
+
+    for _ in range(num_words):
+        
+        response = client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=generated_text,
+            max_tokens=1,
+            temperature=temperature,
+            logprobs=5,
+        )
+        token = response.choices[0].text.strip()
+        generated_text += " " + token
+
+        #logprobs = response.choices[0].logprobs.top_logprobs[0].items()
+        #top_tokens = [token for token, prob in logprobs]
+        #top_tokens_list.append(top_tokens)
+        response_list.append(generated_text)
+
+    return response_list
+
+
+# Streamlit app
+def main():
+    
+    #client = create_openai_client(st.secrets["my_secrets"]["openai_key"])
+
+    st.title("Predicting the Next Word")
+    st.subheader("Autoregression Demonstration")
+
+    st.write("""
+    This application demonstrates the autoregressive mechanism in Large Language Models (LLM) by predicting the next word(s) based on a given input text.
+    """)
+
+    # Number of words to predict
+    num_words = st.slider("Number of words to predict:", 1, 20, 5)
+
+    # Temperature setting
+    temperature = st.slider("Model Temperature:", 0.0, 1.0, 0.7, 0.1)
+
+    # Input text
+    input_text = st.text_area("Enter your text below:", "The best thing of AI is its ability to")
+
+    if st.button("Generate Next Words"):
+        if input_text.strip():
+            with st.spinner("Generating..."):
+                response_list = generate_text(input_text, num_words, temperature)
+#            st.success("Generated Text:")
+            for r in response_list:
+                st.write(r)
+        else:
+            st.error("Please enter some input text to generate predictions.")
+
+
+if __name__ == "__main__":
+    main()
